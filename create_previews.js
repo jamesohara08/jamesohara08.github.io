ad1a03f284f610c53c657fc123929740384095ce
jamesohara08 <44952453+jamesohara08@users.noreply.github.com>
2025-02-21 23:34:55 -0500

    Fixing blog listing

--- create_previews.js
+++ create_previews.js
@@ -5,11 +5,25 @@ function getDivFromHTML(htmlString, divId) {
   return targetDiv;
 }
 
-async function fetchPost(){
-	const response = await fetch("https://jamesohara08.github.io/home/post/KevinLong.html");
+async function fetchPost(url){
+	const response = await fetch(url);
 	const data = await response.text();
 	var post_content = getDivFromHTML(data, 'post');
 	document.getElementById('content').appendChild(post_content);
+	return true;
 }
 
-fetchPost();
+async function buildHomepage(){
+	const response = await fetch("https://raw.githubusercontent.com/jamesohara08/home/refs/heads/main/bloglist.csv");
+	const data = await response.text();
+	var lines = data.split('\n');
+	for(const line of lines){
+		const filename = line.split(',')[0];
+		if(filename != 'html_name'){
+			console.log(filename);
+			const url = "https://jamesohara08.github.io/home/post/" + filename;
+			fetchPost(url);
+		}
+	}
+}
+buildHomepage();
