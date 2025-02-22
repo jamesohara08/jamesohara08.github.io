d862a44439408c67a9713921ba3a44d6b2e5d385
jamesohara08 <44952453+jamesohara08@users.noreply.github.com>
2025-02-22 00:39:19 -0500

    Added pagination with headline links

--- create_previews.js
+++ create_previews.js
@@ -9,8 +9,13 @@ async function fetchPost(url){
 	const response = await fetch(url);
 	const data = await response.text();
 	var post_content = getDivFromHTML(data, 'post');
-	document.getElementById('content').appendChild(post_content);
-	return true;
+	const headline = post_content.getElementsByTagName("h1")[0];
+	const headline_text = headline.innerHTML;
+	headline.innerHTML = `<a href="${url}" class="w3-text-indigo">${headline_text}</a>`
+	const hr = document.createElement('hr');
+	hr.style = "border-top: 3px solid #bbb;"
+	post_content.appendChild(hr);
+	return post_content;
 }
 
 async function buildHomepage(){
@@ -22,8 +27,33 @@ async function buildHomepage(){
 		if(filename != 'html_name'){
 			console.log(filename);
 			const url = "https://jamesohara08.github.io/home/post/" + filename;
-			fetchPost(url);
+			const post = await fetchPost(url);
+			posts.push(post);
 		}
 	}
+	const num_of_pages = Math.floor(posts.length / 10) + 1;
+	const pagination_bar = document.getElementById('pagination');
+	for(i=1;i<num_of_pages+1;i++){
+		const page_num = i.toString();
+		const page_btn = document.createElement('button');
+		page_btn.className = 'w3-button';
+		page_btn.setAttribute('onclick',`paginate(${i})`);
+		page_btn.textContent = page_num;
+		pagination_bar.appendChild(page_btn);
+	}
+	paginate(1);
 }
+
+function paginate(page_num){
+	const content = document.getElementById('content');
+	while(content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+	const start_index = (page_num - 1) * 10;
+	for(i=start_index;i<start_index+10;i++){
+		content.appendChild(posts[i]);
+	}
+}
+
+const posts = [];
 buildHomepage();
